e9a5539736f3afebcd1317035e41f32fae0b369f
dvin-22 <154444746+dvin-22@users.noreply.github.com>
2026-08-28 17:44:43 +0900

    chore(build): Spring Boot 프로젝트 및 로컬 개발 환경 설정

---
+++ src/main/kotlin/com/caffeinerhythm/CaffeineRhythmApplication.kt
@@ -0,0 +1,11 @@
+package com.caffeinerhythm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaffeineRhythmApplication
+
+fun main(args: Array<String>) {
+	runApplication<CaffeineRhythmApplication>(*args)
+}
